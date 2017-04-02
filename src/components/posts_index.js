6491892d1352dae108eb6a779e5a77bc5fcb21fa
Syllinger <syllinger@gmail.com>
2017-04-02 17:42:12 -0400

    Add posts reducer and fire action on mount.

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 
-export default () => {
-  return (
-    <div>List of blog posts.</div>
-  );
+import {fetchPosts} from '../actions/index';
+
+class PostsIndex extends React.Component {
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return (
+      <div>List of blog posts</div>
+    );
+  }
 }
+
+/* Refactor to some Redux shorthand
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({fetchPosts}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(PostsIndex);
+*/
+
+export default connect(null, {fetchPosts})(PostsIndex);
