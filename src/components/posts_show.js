fe6609fb544268ef75911cb3ad8fbd3fec1d97d4
Syllinger <syllinger@gmail.com>
2017-04-02 22:42:21 -0400

    Add show post detail page.

---
+++ src/components/posts_show.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+
+import {fetchPost} from '../actions/index';
+
+class PostsShow extends React.Component {
+  componentWillMount() {
+    this.props.fetchPost(this.props.params.id);
+  }
+
+  render() {
+
+    const {post} = this.props;
+
+    if(!post) {
+      return <div></div>;
+    }
+
+    return (
+      <div>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {post: state.posts.post};
+}
+
+export default connect(mapStateToProps, {fetchPost})(PostsShow);
